47f3a6abbb5cff4242f51d6a25006fac50219868
kshitij430 <59320687+kshitij430@users.noreply.github.com>
2022-08-21 17:16:10 +0530

    server file path added

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const socketio = require("socket.io");
 const path = require("path");
 const app = express();
 require("dotenv").config({ path: path.join(__dirname, "..", "config.env") });
-app.use(express.static("../client"));
-const server = app.listen(process.env.PORT, () => {
+app.use(express.static(`${path.join(__dirname, "..", "client")}`));
+const server = app.listen(process.env.PORT, process.env.IP, () => {
   console.log(`server started successfully on ${process.env.PORT}`);
 });
 const io = socketio(server);
